997d14281b57a48b922cdb1b6ad602ba3c36b08f
Akshar-Rathaur <aksharrathaur@gmail.com>
2025-11-26 17:15:56 +0530

    Delete User.java

--- User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    String email, name, role;
-
-    public User(String email, String name, String role) {
-        this.email = email;
-        this.name = name;
-        this.role = role;
-    }
-}
