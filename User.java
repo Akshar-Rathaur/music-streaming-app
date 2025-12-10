58e438418c1124c2e9a088d8eedb177717e38a44
Akshar-Rathaur <aksharrathaur@gmail.com>
2025-12-10 11:12:39 +0530

    Delete User.java

--- User.java
+++
@@ -1,5 +0,0 @@
-public abstract class User {
-    protected String name, email;
-    public User(String name, String email) { this.name=name; this.email=email; }
-    public abstract void dashboard();
-}
